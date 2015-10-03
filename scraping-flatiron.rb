45f906dcacf93e7a5e91498ee42711d165bee44d
talum <tracidini@gmail.com>
2015-10-03 18:42:08 -0400

    Populates hash all at once. Adds more CL functionality and instructions

--- scraping-flatiron.rb
+++ scraping-flatiron.rb
@@ -51,92 +51,122 @@ def add_links_to_hash(student_hash, doc)
   student_hash
 end
 
-def scrape_and_store_each_profile(doc)
-  doc_hash = {}
+def populate_hash(student_hash, doc)
   students_with_links = students_links(doc)
   students_with_links.map do |name, link|
     profile_html = open(link)
     profile_doc = Nokogiri::HTML(profile_html)
-    doc_hash[name] = profile_doc
+    student_hash[name][:tagline] = profile_doc.search('.textwidget h3').text 
+    student_hash[name][:bio] = profile_doc.search('.services p').first.text
+    student_hash[name][:github] = profile_doc.search('.social-icons a')[2].attr('href')
+    student_hash[name][:twitter] = profile_doc.search('.social-icons a')[0].attr('href')
+    student_hash[name][:linkedin] = profile_doc.search('.social-icons a')[1].attr('href')  
   end
-  doc_hash
-end
-
-def populate_hash_with_profile(student_hash, html_hash, attribute, selectors)  
-  #open the link for each student and grab selected element 
-  html_hash.each do |name, html|
-    value = html.search(selectors).text
-    student_hash[name][attribute] = value
-  end  
   student_hash
 end
 
-def populate_hash_with_bio(student_hash, html_hash)
-  html_hash.each do |name, html|
-    value = html.search('.services p').first.text
-    student_hash[name][:bio] = value
-  end  
-  student_hash
-end
+# def populate_hash_with_profile(student_hash, html_hash, attribute, selectors)  
+#   #open the link for each student and grab selected element 
+#   html_hash.each do |name, html|
+#     value = html.search(selectors).text
+#     student_hash[name][attribute] = value
+#   end  
+#   student_hash
+# end
 
-def populate_hash_with_social(student_hash, html_hash, network, index)
-  html_hash.each do |name, html|
-    value = html.search('.social-icons a')[index].attr('href')
-    student_hash[name][network] = value
-  end  
-  student_hash
-end
+# def populate_hash_with_bio(student_hash, html_hash)
+#   html_hash.each do |name, html|
+#     value = html.search('.services p').first.text
+#     student_hash[name][:bio] = value
+#   end  
+#   student_hash
+# end
+
+# def populate_hash_with_social(student_hash, html_hash, network, index)
+#   html_hash.each do |name, html|
+#     value = html.search('.social-icons a')[index].attr('href')
+#     student_hash[name][network] = value
+#   end  
+#   student_hash
+# end
 
 def fill_student_hash(doc)
   students = create_student_hash(doc)
   add_links_to_hash(students, doc)   
-  html_hash = scrape_and_store_each_profile(doc)
-  populate_hash_with_profile(students, html_hash, :tagline, '.textwidget h3') #tagline
-  populate_hash_with_bio(students, html_hash)   #bio
-  populate_hash_with_social(students, html_hash, :github, 2)   #github
-  populate_hash_with_social(students, html_hash, :twitter, 0)   #twitter
-  populate_hash_with_social(students, html_hash, :linkedin, 1)   # linkedin
+  populate_hash(students, doc)
+  #html_hash = scrape_and_store_each_profile(doc)
+  #populate_hash_with_profile(students, html_hash, :tagline, '.textwidget h3') #tagline
+  #populate_hash_with_bio(students, html_hash)   #bio
+  #populate_hash_with_social(students, html_hash, :github, 2)   #github
+  #populate_hash_with_social(students, html_hash, :twitter, 0)   #twitter
+  #populate_hash_with_social(students, html_hash, :linkedin, 1)   # linkedin
 end
 
+def display_student_info(students)
+#make sure that the hash has key. 
+  puts "Enter a student's name to learn more about him or her."
+  name = gets.chomp
+  if students.has_key?(name)
+    puts "Cool. You can view their tagline and bio, visit their profile on Flatiron School or see
+    them on github, twitter, or linkedin. Type switch to enter another student's name."
+    call_student_attributes(students, name)
+  elsif name == back
+    ##escape the loop? 
+  elsif name == exit
+    exit = true
+  else
+    puts "Sorry, don't recognize that name"  
+  end  
+end
+
+def call_student_attributes(students, name)
+  quit = false
+  until quit
+    puts "Enter a command. Tagline, bio, profile, github, twitter, or linkedin. Back to return. Exit to exit"
+    command = gets.chomp.downcase
+    case command
+      when "tagline"
+        puts students[name][:tagline]
+      when "bio"
+        puts students[name][:bio]
+      when "profile"
+        link = students[name][:link]
+        system("open #{link}")
+      when "github"
+        link = students[name][:github]
+        system("open #{link}")
+      when "twitter"
+        link = students[name][:twitter]
+        system ("open #{link}")
+      when "linkedin"
+        link = students[name][:linkedin]
+        system ("open #{link}")
+      when "back"
+        ##escape the loop, ideally to prompt for another student's name.
+      when "exit"
+        quit = true
+      end
+    end
+end
 
 students = fill_student_hash(doc) 
-puts "Welcome to the Flatiron School Web 0915. Which
-student would you like to learn more about?" 
-#other options: list all students. 
-name = gets.chomp
+puts "Welcome to the Flatiron School Web 0915."
 
-#make sure that the hash has key. 
-if students.has_key?(name)
-  puts "Cool. You can view their tagline and bio, visit their profile on Flatiron School or see
-them on github, twitter, or linkedin."
-else
-  puts "Sorry, don't recognize that name"  
-end
-
-#add option to switch from student to student. 
 exit = false
+puts "Learn more about our students. Type help for a list of commands."
 until exit
-  puts "Enter a command. Tagline, bio, profile, github, twitter, or linkedin. Exit to exit"
-  command = gets.chomp.downcase
-  case command
-    when "tagline"
-      puts students[name][:tagline]
-    when "bio"
-      puts students[name][:bio]
-    when "profile"
-      link = students[name][:link]
-      system("open #{link}")
-    when "github"
-      link = students[name][:github]
-      system("open #{link}")
-    when "twitter"
-      link = students[name][:twitter]
-      system ("open #{link}")
-    when "linkedin"
-      link = students[name][:linkedin]
-      system ("open #{link}")
+  puts "Please enter a command."
+  navigate = gets.chomp.downcase
+  case navigate
+    when "help"
+      puts "Here are your options. Type students to see a list of students. Or type info to learn more about each student."
+    when "students"
+      puts students.keys
+    when "info"
+      display_student_info(students)
     when "exit"
       exit = true
   end
 end
+
 puts "See ya!"
